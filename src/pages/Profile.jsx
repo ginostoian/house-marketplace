1a7fe3db01e5b515a99aa07531b89e6a675d3362
Gino Stoian <ginostoian@gmail.com>
2022-05-09 22:39:21 +0100

    add sign out functionality on profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,17 +1,29 @@
 import { getAuth } from 'firebase/auth'
 import { useState, useEffect } from 'react'
+import { useNavigate, Link } from 'react-router-dom'
 
 function Profile() {
     const auth = getAuth()
-    const [user, setUser] = useState(null)
+    const navigate = useNavigate()
 
-    useEffect(() => {
-        setUser(auth.currentUser)
-    }, [])
+    const [formData, setFormData] = useState({
+        name: auth.currentUser.displayName,
+        email: auth.currentUser.email
+    })
+
+    const { name, email } = formData
+
+    const onLogOut = () => {
+        auth.signOut()
+        navigate('/')
+    }
 
     return (
-        <div>
-            {user ? <h1>Welcome back {user.displayName}!</h1> : <h1>Not logged in</h1>}
+        <div className='profile'>
+            <header className="profileHeader">
+                <p className="pageHeader">My Profile</p>
+                <button className="logOut" type='button' onClick={onLogOut}>Log out</button>
+            </header>
         </div>
     )
 }
