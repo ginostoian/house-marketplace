b1418d18410f9d743dac90b3ec9ce8efcc1646e2
Gino Stoian <ginostoian@gmail.com>
2022-06-08 00:59:34 +0300

    save listing into db after upload of images

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -3,6 +3,7 @@
 import { useState, useEffect, useRef } from "react"
 import { getAuth, onAuthStateChanged } from "firebase/auth"
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage"
+import { addDoc, collection, serverTimestamp } from "firebase/firestore"
 import { useNavigate } from "react-router-dom"
 import { toast } from "react-toastify"
 import { v4 as uuidv4 } from "uuid"
@@ -156,9 +157,25 @@ function CreateListing() {
             return
         })
 
-        console.log(imgUrls)
+        const formDataCopy = {
+            ...formData,
+            imgUrls,
+            geolocation,
+            timestamp: serverTimestamp()
+        }
+
+        delete formDataCopy.images
+        delete formDataCopy.address
+        location && (formDataCopy.location = location)
+        !formDataCopy.offer && delete formDataCopy.discountedPrice
+
+        const docRef = await addDoc(collection(db, 'listings'), formDataCopy)
 
         setLoading(false)
+
+        toast.success('Listing is now live')
+
+        navigate(`/category/${formDataCopy.type}/${docRef.id}`)
     }
 
     const onMutate = e => {
