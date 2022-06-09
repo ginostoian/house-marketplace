ca52e8fb95fe7759b043203a6e91061bedb2c637
Gino Stoian <ginostoian@gmail.com>
2022-06-10 01:30:03 +0300

    add route to edit listing

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Category from "./pages/Category"
 import CreateListing from "./pages/CreateListing"
+import EditListing from "./pages/EditListing"
 import Listing from "./pages/Listing"
 import Contact from "./pages/Contact"
 
@@ -31,6 +32,7 @@ function App() {
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/create-listing' element={<CreateListing />} />
+          <Route path='/edit-listing/:listingId' element={<EditListing />} />
           <Route path='/contact/:landlordId' element={<Contact />} />
         </Routes>
 
---
+++ src/pages/EditListing.jsx
@@ -0,0 +1,10 @@
+
+function EditListing() {
+    return (
+        <div>
+            Edit
+        </div>
+    )
+}
+
+export default EditListing
