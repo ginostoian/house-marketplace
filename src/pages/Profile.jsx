0e310eacde31fb208cd891f7262bcb4a7039362e
Gino Stoian <ginostoian@gmail.com>
2022-06-10 00:50:41 +0300

    fetch all the listings of the signed in user and store them in state

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import { getAuth, updateProfile } from 'firebase/auth'
-import { doc, updateDoc } from 'firebase/firestore'
-import { useState } from 'react'
+import { doc, updateDoc, collection, getDocs, query, where, orderBy, deleteDoc } from 'firebase/firestore'
+import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import { Link } from 'react-router-dom'
@@ -18,8 +18,34 @@ function Profile() {
         name: auth.currentUser.displayName,
         email: auth.currentUser.email
     })
+    const [listings, setListings] = useState(null)
+    const [loading, setLoading] = useState(true)
 
     const { name, email } = formData
+
+    useEffect(() => {
+        const fetchUserListings = async () => {
+            const listingsRef = collection(db, 'listings')
+
+            const q = query(listingsRef, where('userRef', '==', auth.currentUser.uid), orderBy('timestamp', 'desc'))
+
+            const querySnap = await getDocs(q)
+
+            const listings = []
+
+            querySnap.forEach((doc) => {
+                return listings.push({
+                    id: doc.id,
+                    data: doc.data()
+                })
+            })
+
+            setListings(listings)
+            setLoading(false)
+        }
+
+        fetchUserListings()
+    }, [auth.currentUser.uid])
 
     const onLogOut = () => {
         auth.signOut()
